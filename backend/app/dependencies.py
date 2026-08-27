0c03c38002c302100902a8767802f47a96d35f35
cugarete <60232732+aryarifki@users.noreply.github.com>
2026-08-27 22:49:57 +0700

    Create dependencies.py

---
+++ backend/app/dependencies.py
@@ -0,0 +1,12 @@
+"""FastAPI dependency injections."""
+
+from __future__ import annotations
+
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+
+DbSession = Annotated[Session, Depends(get_db)]
